b6a021687d8a166f1dc6f6e77a5e24f8d4831b69
wangliang <1991wangliang@gmail.com>
2017-07-06 11:47:47 +0800

    兼容spring3 调整启动监听器

--- lorne-tx-core/src/main/java/com/lorne/tx/listener/TransactionSocketListener.java
+++ lorne-tx-core/src/main/java/com/lorne/tx/listener/TransactionSocketListener.java
@@ -1,18 +1,19 @@
-package com.lorne.tx.springcloud.listener;
+package com.lorne.tx.listener;
 
 import com.lorne.tx.mq.service.NettyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by lorne on 2017/7/1.
  */
 @Component
-public class TransactionSocketListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+public class TransactionSocketListener implements ApplicationContextAware {
 
 
     private Logger logger = LoggerFactory.getLogger(TransactionSocketListener.class);
@@ -20,9 +21,11 @@ public class TransactionSocketListener implements ApplicationListener<EmbeddedSe
     @Autowired
     private NettyService nettyService;
 
+
     @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+    public void setApplicationContext(ApplicationContext event) throws BeansException {
         nettyService.start();
         logger.info("socket-start..");
     }
+
 }
