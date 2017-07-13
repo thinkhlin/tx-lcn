b73146cf46a4d5399380e14b81419365ce05dcc1
wangliang <1991wangliang@gmail.com>
2017-07-13 10:48:33 +0800

    添加注释

--- lorne-tx-core/src/main/java/com/lorne/tx/service/impl/TransactionServerFactoryServiceImpl.java
+++ lorne-tx-core/src/main/java/com/lorne/tx/service/impl/TransactionServerFactoryServiceImpl.java
@@ -42,11 +42,16 @@ public class TransactionServerFactoryServiceImpl implements TransactionServerFac
 
     public TransactionServer createTransactionServer(TxTransactionInfo info) throws Throwable {
 
+
+
+
+
+        /*********补偿事务处理逻辑*开始***********/
         /** 事务补偿业务处理中**/
         if(CompensateServiceImpl.COMPENSATE_KEY.equals(info.getTxGroupId())){
-            //控制返回业务数据，但事务回滚。
+            //控制返回业务数据，但让其事务回滚。第一次执行时，需要启用线程控制事务，后面的事务与开始启动的事务事务嵌套即可。然后通过开始事务统一回滚。
+            //因此执行业务过程中时的事务与txInServiceTransactionServer处理一致
             if(TxTransactionLocal.current()!=null){
-                //事务合并到开始业务内 与 txInServiceTransactionServer处理一致
                 return txInServiceTransactionServer;
             }else{
                 return txRunningCompensateTransactionServer;
@@ -55,9 +60,18 @@ public class TransactionServerFactoryServiceImpl implements TransactionServerFac
 
         /** 事务补偿业务开始标示**/
         if(info.getCompensate()!=null){
-            //正常处理，同模下将依旧执行该方法
+            //正常处理，同模下将依旧执行方法。
             return txStartCompensateTransactionServer;
         }
+
+        /*********补偿事务处理逻辑*结束***********/
+
+
+
+
+
+
+        /*********本地事务处理逻辑*开始***********/
 
         /** 当都是空的时候表示是一般的业务处理。这里不做操作 **/
         if (StringUtils.isEmpty(info.getTxGroupId())
@@ -67,14 +81,22 @@ public class TransactionServerFactoryServiceImpl implements TransactionServerFac
             return txDefaultTransactionServer;
         }
 
+        /** 本地事务嵌套 **/
         if (info.getTransactionLocal() != null) {
             return txDefaultTransactionServer;
         }
 
+        /*********本地事务处理逻辑*结束***********/
+
+
+
+
+
+        /*********分布式事务处理逻辑*开始***********/
 
         /** 尽当Transaction注解不为空，其他都为空时。表示分布式事务开始启动 **/
         if (info.getTransaction() != null && info.getTxTransactionLocal() == null && StringUtils.isEmpty(info.getTxGroupId())) {
-            //检查socket通讯是否正常
+            //检查socket通讯是否正常 （当启动事务的主业务方法执行完以后，再执行其他业务方法时将进入txInServiceTransactionServer业务处理）
             if (nettyService.checkState()) {
                 return txStartTransactionServer;
             } else {
@@ -84,7 +106,7 @@ public class TransactionServerFactoryServiceImpl implements TransactionServerFac
 
         /** 分布式事务已经开启，业务进行中 **/
         if (info.getTxTransactionLocal() != null || StringUtils.isNotEmpty(info.getTxGroupId())) {
-            //检查socket通讯是否正常
+            //检查socket通讯是否正常 （第一次执行时启动txRunningTransactionServer的业务处理控制，然后嵌套调用其他事务的业务方法时都并到txInServiceTransactionServer业务处理下）
             if (nettyService.checkState()) {
                 if(info.getTxTransactionLocal() != null){
                     return txInServiceTransactionServer;
@@ -97,6 +119,9 @@ public class TransactionServerFactoryServiceImpl implements TransactionServerFac
             }
 
         }
+        /*********分布式事务处理逻辑*结束***********/
+
+
 
 
         return txDefaultTransactionServer;
