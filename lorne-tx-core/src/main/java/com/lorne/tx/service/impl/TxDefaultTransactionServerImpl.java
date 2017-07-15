b5790870af85c31c425bdad772d4848e8aa0cd3e
wangliang <1991wangliang@gmail.com>
2017-07-15 21:17:17 +0800

    去掉本地事务

--- lorne-tx-core/src/main/java/com/lorne/tx/service/impl/TxDefaultTransactionServerImpl.java
+++ lorne-tx-core/src/main/java/com/lorne/tx/service/impl/TxDefaultTransactionServerImpl.java
@@ -4,12 +4,7 @@ import com.lorne.tx.bean.TransactionLocal;
 import com.lorne.tx.bean.TxTransactionInfo;
 import com.lorne.tx.service.TransactionServer;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * Created by lorne on 2017/6/8.
@@ -17,9 +12,6 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 @Service(value = "txDefaultTransactionServer")
 public class TxDefaultTransactionServerImpl implements TransactionServer {
 
-
-    @Autowired
-    private PlatformTransactionManager txManager;
 
     @Override
     public Object execute(ProceedingJoinPoint point, TxTransactionInfo info) throws Throwable {
@@ -30,18 +22,7 @@ public class TxDefaultTransactionServerImpl implements TransactionServer {
             TransactionLocal.setCurrent(transactionLocal);
             hasStart = true;
         }
-        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-        TransactionStatus status = txManager.getTransaction(def);
-
-        Object obj = null;
-        try {
-            obj =  point.proceed();
-            txManager.commit(status);
-        }catch (Throwable e){
-            txManager.rollback(status);
-            throw e;
-        }
+        Object obj = point.proceed();
         if (hasStart) {
             TransactionLocal.setCurrent(null);
         }
