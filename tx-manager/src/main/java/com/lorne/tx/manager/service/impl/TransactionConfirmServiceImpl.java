5cb3b9dc556190ee8f868990f1c5a3f2f23df2a8
wangliang <1991wangliang@gmail.com>
2017-07-26 18:24:06 +0800

    回滚直接通知

--- tx-manager/src/main/java/com/lorne/tx/manager/service/impl/TransactionConfirmServiceImpl.java
+++ tx-manager/src/main/java/com/lorne/tx/manager/service/impl/TransactionConfirmServiceImpl.java
@@ -1,7 +1,6 @@
 package com.lorne.tx.manager.service.impl;
 
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.lorne.core.framework.utils.KidUtils;
 import com.lorne.core.framework.utils.task.ConditionUtils;
@@ -129,78 +128,93 @@ public class TransactionConfirmServiceImpl implements TransactionConfirmService 
      * @param checkSate
      */
     private boolean transaction(TxGroup txGroup, final int checkSate) {
-        CountDownLatchHelper<Boolean> countDownLatchHelper = new CountDownLatchHelper<>();
-        for (final TxInfo txInfo : txGroup.getList()) {
-            if(txInfo.getIsGroup()==0) {
-                countDownLatchHelper.addExecute(new IExecute<Boolean>() {
-                    @Override
-                    public Boolean execute() {
-                        final JSONObject jsonObject = new JSONObject();
-                        jsonObject.put("a", "t");
-                        jsonObject.put("c", checkSate);
-                        jsonObject.put("t", txInfo.getKid());
-                        String key = KidUtils.generateShortUuid();
-                        jsonObject.put("k", key);
-                        final Task task = ConditionUtils.getInstance().createTask(key);
+        if (checkSate == 1) {
+            CountDownLatchHelper<Boolean> countDownLatchHelper = new CountDownLatchHelper<>();
+            for (final TxInfo txInfo : txGroup.getList()) {
+                if (txInfo.getIsGroup() == 0) {
+                    countDownLatchHelper.addExecute(new IExecute<Boolean>() {
+                        @Override
+                        public Boolean execute() {
+                            final JSONObject jsonObject = new JSONObject();
+                            jsonObject.put("a", "t");
+                            jsonObject.put("c", checkSate);
+                            jsonObject.put("t", txInfo.getKid());
+                            String key = KidUtils.generateShortUuid();
+                            jsonObject.put("k", key);
+                            final Task task = ConditionUtils.getInstance().createTask(key);
 
-                        ScheduledFuture future = executorService.schedule(new Runnable() {
-                            @Override
-                            public void run() {
-                                task.setBack(new IBack() {
-                                    @Override
-                                    public Object doing(Object... objs) throws Throwable {
-                                        return "-2";
-                                    }
-                                });
-                                task.signalTask();
+                            ScheduledFuture future = executorService.schedule(new Runnable() {
+                                @Override
+                                public void run() {
+                                    task.setBack(new IBack() {
+                                        @Override
+                                        public Object doing(Object... objs) throws Throwable {
+                                            return "-2";
+                                        }
+                                    });
+                                    task.signalTask();
+                                }
+                            }, txManagerService.getDelayTime() * 3, TimeUnit.SECONDS);
+
+                            threadPool.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    awaitSend(task, txInfo, jsonObject);
+                                }
+                            });
+                            task.awaitTask();
+
+                            if (!future.isDone()) {
+                                future.cancel(false);
                             }
-                        }, txManagerService.getDelayTime() * 3, TimeUnit.SECONDS);
 
-                        threadPool.execute(new Runnable() {
-                            @Override
-                            public void run() {
-                                awaitSend(task, txInfo, jsonObject);
+
+                            try {
+                                String data = (String) task.getBack().doing();
+                                // 1  成功 0 失败 -1 task为空 -2 超过
+                                boolean res = "1".equals(data);
+
+                                if ("1".equals(data) || "0".equals(data)) {
+                                    txInfo.setNotify(1);
+                                }
+
+                                return res;
+                            } catch (Throwable throwable) {
+                                throwable.printStackTrace();
+                                return false;
+                            } finally {
+                                task.remove();
                             }
-                        });
-                        task.awaitTask();
-
-                        if (!future.isDone()) {
-                            future.cancel(false);
                         }
-
-
-                        try {
-                            String data = (String) task.getBack().doing();
-                            // 1  成功 0 失败 -1 task为空 -2 超过
-                            boolean res = "1".equals(data);
-
-                            if ("1".equals(data) || "0".equals(data)) {
-                                txInfo.setNotify(1);
-                            }
-
-                            return res;
-                        } catch (Throwable throwable) {
-                            throwable.printStackTrace();
-                            return false;
-                        } finally {
-                            task.remove();
-                        }
-                    }
-                });
+                    });
+                }
             }
+
+            List<Boolean> hasOks = countDownLatchHelper.execute().getData();
+
+            boolean hasOk = true;
+            for (boolean bl : hasOks) {
+                if (bl == false) {
+                    hasOk = false;
+                    break;
+                }
+            }
+            logger.info("--->" + hasOk + ",group:" + txGroup.getGroupId() + ",state:" + checkSate + ",list:" + txGroup.toJsonString());
+            return hasOk;
+        }else{
+            //回滚操作只发送通过不需要等待确认
+            for (TxInfo txInfo : txGroup.getList()) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("a", "t");
+                jsonObject.put("c", checkSate);
+                jsonObject.put("t", txInfo.getKid());
+                String key = KidUtils.generateShortUuid();
+                jsonObject.put("k", key);
+                SocketUtils.sendMsg( txInfo.getChannel(),jsonObject.toString());
+            }
+            return true;
         }
 
-        List<Boolean> hasOks = countDownLatchHelper.execute().getData();
-
-        boolean hasOk = true;
-        for (boolean bl : hasOks) {
-            if (bl == false) {
-                hasOk =  false;
-                break;
-            }
-        }
-        logger.info("--->"+hasOk+",group:"+txGroup.getGroupId()+",state:"+checkSate+",list:" +txGroup.toJsonString());
-        return hasOk;
     }
 
 
