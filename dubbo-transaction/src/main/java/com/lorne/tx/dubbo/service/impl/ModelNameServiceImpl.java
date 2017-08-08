16c8ff6074cb10adbdea922ba1b9bf03cef94b5b
wangliang <1991wangliang@gmail.com>
2017-08-08 16:59:09 +0800

    获取端口号

--- dubbo-transaction/src/main/java/com/lorne/tx/dubbo/service/impl/ModelNameServiceImpl.java
+++ dubbo-transaction/src/main/java/com/lorne/tx/dubbo/service/impl/ModelNameServiceImpl.java
@@ -1,14 +1,14 @@
 package com.lorne.tx.dubbo.service.impl;
 
 import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.remoting.exchange.ExchangeServer;
-import com.alibaba.dubbo.rpc.protocol.dubbo.DubboProtocol;
+import com.alibaba.dubbo.config.ProviderConfig;
 import com.lorne.core.framework.utils.encode.MD5Util;
 import com.lorne.tx.service.ModelNameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by lorne on 2017/7/12.
@@ -20,19 +20,30 @@ public class ModelNameServiceImpl implements ModelNameService {
     @Autowired
     private  ApplicationConfig applicationConfig;
 
+    @Autowired
+    private ProviderConfig providerConfig;
+
+
     @Override
     public String getModelName() {
         return  applicationConfig.getName();
     }
 
+    private String getIp(){
+        String host = null;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return host;
+    }
+
     @Override
     public String getUniqueKey() {
-        DubboProtocol dubboProtocol =  DubboProtocol.getDubboProtocol();
-        Collection<ExchangeServer> servers =  dubboProtocol.getServers();
-        String address = "";
-        for(ExchangeServer server:servers){
-            address+=server.getLocalAddress();
-        }
-        return MD5Util.md5(address);
+        String address = getIp()+providerConfig.getPort();
+        return  MD5Util.md5(address);
     }
+
+
 }
