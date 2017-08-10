8c6aacd1d55a0909d19eead8dd86b6fe4dbb861e
wangliang <1991wangliang@gmail.com>
2017-08-10 23:13:39 +0800

    优化代理连接池

--- lorne-tx-core/src/main/java/com/lorne/tx/compensate/repository/JdbcTransactionRecoverRepository.java
+++ lorne-tx-core/src/main/java/com/lorne/tx/compensate/repository/JdbcTransactionRecoverRepository.java
@@ -7,6 +7,7 @@ import com.lorne.tx.utils.SerializerUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -37,6 +38,7 @@ public class JdbcTransactionRecoverRepository implements TransactionRecoverRepos
     private String unique;
 
     @Autowired
+    @Qualifier("compensateDataSource")
     private DataSource compensateDataSource;
 
     @Override
